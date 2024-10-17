dd4db53d64a976bdfebe6a5adc7a57e47a509acd
SoliTheUnholy <soheilzss82@gmail.com>
2024-10-17 19:14:37 +0330

    sabkar khare

--- app/home/services/page.tsx
+++ app/home/services/page.tsx
@@ -1,19 +1,189 @@
+"use client"
 import { AutoCarousel } from "@/components/autoCarousel";
-import { Card } from "@/components/ui/card";
-export default function services() {
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+
+export default function Services() {
+  const [tb, setTb] = useState("distance");
   return (
-    <div className="mx-4 grid gap-4 sm:mx-12 xl:mx-24">
+    <div className="mx-auto grid w-[95vw] gap-4 sm:w-fit xl:mx-24">
       <div className="relative mt-4 h-auto animate-fade-in-left overflow-hidden rounded-xl">
         <div className="h-[20vh]">
           <AutoCarousel />
         </div>
       </div>
-      <div className="">
+      <div className="flex items-center justify-center">
         <Card className="grid animate-fade-in-up gap-2 border-none bg-muted/75 p-4 lg:p-8">
           <h2 className="text-lg font-black text-foreground">خدمات</h2>
-          <div
-            className={`flex justify-center gap-4 gap-y-8 sm:grid-cols-3 lg:grid-cols-4 lg:gap-8`}
-          ></div>
+          <Tabs dir="rtl" defaultValue="distance" value={tb} className="">
+            <TabsList className="grid w-full grid-cols-5">
+              <TabsTrigger
+                onClick={() => {
+                  setTb("distance");
+                }}
+                value="distance"
+              >
+                مسافت
+              </TabsTrigger>
+              <TabsTrigger
+                onClick={() => {
+                  setTb("car");
+                }}
+                value="car"
+              >
+                خودرو
+              </TabsTrigger>
+              <TabsTrigger
+                onClick={() => {
+                  setTb("location");
+                }}
+                value="location"
+              >
+                موقعیت
+              </TabsTrigger>
+              <TabsTrigger
+                onClick={() => {
+                  setTb("details");
+                }}
+                value="details"
+              >
+                مشخصات
+              </TabsTrigger>
+              <TabsTrigger
+                onClick={() => {
+                  setTb("status");
+                }}
+                value="status"
+              >
+                وضعیت
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="distance">
+              <Card>
+                <CardHeader>
+                  <CardTitle>مسافت</CardTitle>
+                  <CardDescription>
+                    شهری یا بین شهری بودن سفارش را مشخص کنید.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2"></CardContent>
+                <CardFooter className="flex flex-row justify-end">
+                  <Button
+                    onClick={() => {
+                      setTb("car");
+                    }}
+                    className=""
+                  >
+                    مرحله بعد
+                  </Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="car">
+              <Card>
+                <CardHeader>
+                  <CardTitle>نوع خودرو</CardTitle>
+                  <CardDescription>نوع خودرو را مشخص کنید. </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2"></CardContent>
+                <CardFooter className="flex flex-row justify-between">
+                  <Button
+                    onClick={() => {
+                      setTb("distance");
+                    }}
+                    className=""
+                  >
+                    مرحله قبل
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setTb("location");
+                    }}
+                    className=""
+                  >
+                    مرحله بعد
+                  </Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="location">
+              <Card>
+                <CardHeader>
+                  <CardTitle>موقعیت مکانی</CardTitle>
+                  <CardDescription>
+                    جزئیات موقعیت مکانی را مشخص کنید.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2"></CardContent>
+                <CardFooter className="flex flex-row justify-between">
+                  <Button
+                    onClick={() => {
+                      setTb("car");
+                    }}
+                    className=""
+                  >
+                    مرحله قبل
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setTb("details");
+                    }}
+                    className=""
+                  >
+                    مرحله بعد
+                  </Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="details">
+              <Card>
+                <CardHeader>
+                  <CardTitle>جزئیات سفارش</CardTitle>
+                  <CardDescription>جزئیات سفارش را وارد کنید. </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2"></CardContent>
+                <CardFooter className="flex flex-row justify-between">
+                  <Button
+                    onClick={() => {
+                      setTb("location");
+                    }}
+                    className=""
+                  >
+                    مرحله قبل
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setTb("status");
+                    }}
+                    className=""
+                  >
+                    مرحله بعد
+                  </Button>
+                </CardFooter>
+              </Card>
+            </TabsContent>
+            <TabsContent value="status">
+              <Card>
+                <CardHeader>
+                  <CardTitle>وضعیت سفارش</CardTitle>
+                  <CardDescription>
+                    در انتظار تایید اوپراتور بمانید.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-2"></CardContent>
+                <CardFooter className="flex flex-row justify-between"></CardFooter>
+              </Card>
+            </TabsContent>
+          </Tabs>
         </Card>
       </div>
     </div>
